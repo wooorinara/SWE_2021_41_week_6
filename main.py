65162222fe791ceb9b366d812c37ffbeb3619ae7
wooorinara <99887813+wooorinara@users.noreply.github.com>
2024-04-11 21:05:27 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-Hello World
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+# YOUR ANSWER
+    n = len(nums)
+    for i in range(n):
+        for j in range(i +1, n):
+            if nums[i] + nums[j] == target:
+                return [i,j]
+    return []
+
+nums_input = input("Input: ")
+nums = [int(num) for num in nums_input.split(",")]
+target = int(input("Target2: "))
+print(twoSum(nums, target))
